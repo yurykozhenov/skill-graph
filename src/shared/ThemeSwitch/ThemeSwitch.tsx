451f58833c01b302c6da7db1e740d2e252dbd407
Yury Kozhenov <yurykozhenov@gmail.com>
2019-11-25 02:53:00 +0200

    Make primary color brighter for dark theme

--- src/shared/ThemeSwitch/ThemeSwitch.tsx
+++ src/shared/ThemeSwitch/ThemeSwitch.tsx
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react';
 type Theme = 'light' | 'dark';
 
 const lightTheme = {
+  primaryColor: 'rgb(170, 0, 170)',
   bgColor: '#ffffff',
   textColor: 'rgba(0, 0, 0, 0.87)',
 };
 
 const darkTheme = {
+  primaryColor: 'rgb(190, 0, 190)',
   bgColor: '#111111',
   textColor: 'rgba(255, 255, 255, 0.87)',
 };
@@ -22,6 +24,10 @@ function ThemeSwitch() {
 
     let themeConfig = theme === 'dark' ? darkTheme : lightTheme;
 
+    document.documentElement.style.setProperty(
+      '--primary-color',
+      themeConfig.primaryColor
+    );
     document.documentElement.style.setProperty(
       '--background-color',
       themeConfig.bgColor
